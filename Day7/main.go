800a30612aa69cece68dd66bc0cab964b8b6e6f6
Edward Suarez <edsuarez0299@gmail.com>
2023-06-28 00:19:20 -0500

    [feat] Add the solution related to Day7

---
+++ Day7/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+	reverseArray(arr, n)
+}
+func reverseArray(arr []int32, n int32) {
+	var newArr = make([]int, n)
+	for i, v := range arr {
+		newArr[i] = int(v)
+	}
+	newArr = reverseInts(newArr)
+	result := ""
+	for _, v := range newArr {
+		if len(result) > 0 {
+			result += " "
+		}
+		result += strconv.Itoa(v)
+	}
+	fmt.Println(result)
+}
+func reverseInts(input []int) []int {
+	if len(input) == 0 {
+		return input
+	}
+	return append(reverseInts(input[1:]), input[0])
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
